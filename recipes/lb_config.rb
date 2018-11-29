ce01a7d855f28b5b0628c44b0b2ede71d511e09c
Steven Spriggs <>
2018-11-29 11:37:35 -0500

    fix typo

--- recipes/lb_config.rb
+++ recipes/lb_config.rb
@@ -30,5 +30,5 @@ end
 
 link File.join(conf_dir,'sites-enabled','000-default.conf') do
   to File.join(conf_dir,'sites-available','000-default.conf')
-  notifies :restart, service_name)
+  notifies :restart, service_name
 end
