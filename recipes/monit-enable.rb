c9312dd63f17eebac2d3288668cbe2807224e2e0
Steven Spriggs <>
2019-09-18 11:06:53 -0400

    add comment

--- recipes/monit-enable.rb
+++ recipes/monit-enable.rb
@@ -1,3 +1,6 @@
+# this is here because of the "skip_service": true in the monit::service recipe
+# ubuntu18.04 upgrade issue with chef 12 not having the update-rc.d -n option
+# work around found at https://bugs.launchpad.net/ubuntu/+source/chef/+bug/1766786
 service 'monit' do
   provider Chef::Provider::Service::Systemd
   action [:enable, :start]
