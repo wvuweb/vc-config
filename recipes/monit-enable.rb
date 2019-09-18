808698dff8dfc9e2cbd005f2cb9aba7115ee7f6d
Steven Spriggs <>
2019-09-18 10:47:37 -0400

    try adding subscribes

--- recipes/monit-enable.rb
+++ recipes/monit-enable.rb
@@ -1,4 +1,7 @@
 service 'monit' do
   provider Chef::Provider::Service::Systemd
   action [:enable, :start]
+  subscribes :restart, "template[#{node['monit']['conf_file']}]", :delayed
+  subscribes :restart, 'template[/etc/default/monit]', :delayed
+  supports status: true
 end
